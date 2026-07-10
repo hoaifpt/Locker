c1b80c3e21f360f33d189aba79867bcabc0efb05
hoaifpt <hoaittse184132@fpt.edu.vn>
2026-07-11 00:44:17 +0700

    app

--- mobile/lib/core/constants/environment.dart
+++ mobile/lib/core/constants/environment.dart
@@ -5,21 +5,8 @@ class Environment {
 
   static const String _api = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: '',
+    defaultValue: 'https://api.hoaitran.online/api',
   );
 
-  static String get apiBaseUrl {
-    // Nếu truyền --dart-define thì dùng
-    if (_api.isNotEmpty) {
-      return _api;
-    }
-
-    // Local
-    if (kIsWeb) {
-      return 'http://localhost:5000/api';
-    }
-
-    // Android Emulator
-    return 'http://10.0.2.2:5000/api';
-  }
+  static String get apiBaseUrl => _api;
 }

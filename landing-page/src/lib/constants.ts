f003f0a26cf2afbb2663b9c40dbbe4ed2424c64f
duckhynh <phamduchung888888@gmail.com>
2026-08-13 11:23:20 +0700

    chore: update APK download link

--- landing-page/src/lib/constants.ts
+++ landing-page/src/lib/constants.ts
@@ -1,5 +1,5 @@
 // Google Play Store URL
-export const GOOGLE_PLAY_URL = 'https://drive.usercontent.google.com/download?id=1RS3aaH23jjqrAUR8DJF_ExLBpMdSKQbr&export=download&authuser=0&confirm=t&uuid=8d4f1336-8bab-4fd2-8a78-cfcd3d6694e4&at=AFYLz4MmK2FvA0tA361OhbvQgfQ0:1786592125612';
+export const GOOGLE_PLAY_URL = 'https://drive.usercontent.google.com/download?id=1RS3aaH23jjqrAUR8DJF_ExLBpMdSKQbr&export=download&authuser=0&confirm=t&uuid=124247e1-e4dd-4c02-a675-506b2ec94a1f&at=AFYLz4M_SHPfpoTGvrTNroO5tNIi:1786594907900';
 
 export const APP_NAME = 'E-BOX';
 export const APP_TAGLINE = 'CHỦ ĐỘNG MỌI LÚC – TIỆN LỢI MỌI NƠI';
